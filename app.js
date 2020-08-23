007edb32395bbdf91dff791be3bcfb676a7e4a30
MOSKALENKO Nikolay <Nikolay.MOSKALENKO@raiffeisen.ru>
2020-08-23 05:47:09 +0300

    финальная версия прототипа

--- app.js
+++ app.js
@@ -1,257 +1,385 @@
-'use strict';
-function Median(data) {
-  return Quartile_50(data);
-}
+(() => {
+  'use strict';
 
-function Quartile_25(data) {
-  return Quartile(data, 0.25);
-}
+  let clusterer = null;
+  const competition = {};
+  const competitionByLocation = {};
+  let compMin = 0;
+  let compMax = 0;
+  let compScale = null;
+  let scoreLevelScale = null
 
-function Quartile_50(data) {
-  return Quartile(data, 0.5);
-}
-
-function Quartile_75(data) {
-  return Quartile(data, 0.75);
-}
-
-function Quartile(data, q) {
-  data=Array_Sort_Numbers(data);
-  var pos = ((data.length) - 1) * q;
-  var base = Math.floor(pos);
-  var rest = pos - base;
-  if( (data[base+1]!==undefined) ) {
-    return data[base] + rest * (data[base+1] - data[base]);
-  } else {
-    return data[base];
-  }
-}
-
-function Array_Sort_Numbers(inputarray){
-  return inputarray.sort(function(a, b) {
-    return a - b;
-  });
-}
-
-function Array_Sum(t){
-   return t.reduce(function(a, b) { return a + b; }, 0); 
-}
-
-function Array_Average(data) {
-  return Array_Sum(data) / data.length;
-}
-
-function Array_Stdev(tab){
-   var i,j,total = 0, mean = 0, diffSqredArr = [];
-   for(i=0;i<tab.length;i+=1){
-       total+=tab[i];
-   }
-   mean = total/tab.length;
-   for(j=0;j<tab.length;j+=1){
-       diffSqredArr.push(Math.pow((tab[j]-mean),2));
-   }
-   return (Math.sqrt(diffSqredArr.reduce(function(firstEl, nextEl){
-            return firstEl + nextEl;
-          })/tab.length));  
-}
-
-const openModal = async (inn) => {
-  //alert(inn)
-  console.log(inn);
-  
-  $('#modalLabel').text(`ИНН: ${inn}`);
-
-  let url = `https://hackathon.graphometrica.ai/explanations/${inn}.html`//`explanations/${inn}.html`;
-  //const html = await fetch(`explanations/${inn}.html`).then(r=>r.text());
-
-    $('.modal').on('shown.bs.modal',function(){      //correct here use 'shown.bs.modal' event which 
-      $(this).find('iframe').attr('src', url)
+  const calculateCompetition = (data) => {
+    data.forEach(i=> {
+      
+      if (!competition[i.okved_code]) {
+        competition[i.okved_code]=[getPos(i.location)]
+      }else {
+        competition[i.okved_code].push(getPos(i.location))
+      }
     })
-  
-  // $('#companyExp').attr('src', `explanations/${inn}.html`);
-  // $('#companyExp').removeClass('invisible');
-  
-  $('#companyModal').modal();
 
-}
+    data.forEach(i=> calculateCompetitionByGeo(i))
 
+    
+    let compLevels = Object.keys(competitionByLocation).map(key=>competitionByLocation[key]);
 
+    compMin = d3.min(compLevels);
+    compMax = d3.max(compLevels);
 
-const main = async () => {
-
- let source = await fetch('https://hackathon.graphometrica.ai/api/getAll').then(r => r.json());
-
- source = source.filter(i=>i.location).map(i=> {
-  let location = i.location;
-  
-  if (location.indexOf(', ') >= 0) location = location.split(', ')[0]
-  return {...i, location: location}
-})
-  
-  let preds = [...new Set(source.map(i=>i.preds))]
-  let k = 0;//Quartile(preds, 0.3);
-
-  const filterByScore = (data) => {    
-    return data.filter(i=> i.preds > 0.1).sort((a,b) => a.preds-b.preds)
+    compScale = d3.scaleLinear()
+        .domain([compMin, compMax])
+        .rangeRound([0, 3])
+            
   }
 
-
-source = filterByScore(source);
-
- preds = [...new Set(source.map(i=>i.preds))]
-
-  const predMin = d3.min(preds)
-  const predMax = d3.min(preds)
-
-  
-  
-
-  // var scale = d3.scaleLinear()
-  // .domain([predMin, predMax])
-  // .range([1, 5]);
-
-  
-  
-  const colorFn = d3.scaleSequential()
-  .interpolator(d3.interpolatePurples)
-  .domain([predMin/10, 0.6 ]);
-  
-   //.range(d3.interpolateBlues)
-  // .interpolate(d3.interpolateRgb.gamma(2.2))
-  
-
-  function RGBToHex(rgb) {
-    
-    let sep = rgb.indexOf(",") > -1 ? "," : " ";
-    
-    rgb = rgb.substr(4).split(")")[0].split(sep);
-  
-    let r = (+rgb[0]).toString(16),
-        g = (+rgb[1]).toString(16),
-        b = (+rgb[2]).toString(16);
-  
-    if (r.length == 1)
-      r = "0" + r;
-    if (g.length == 1)
-      g = "0" + g;
-    if (b.length == 1)
-      b = "0" + b;
-  
-    return "#" + r + g + b;
+  const getCompetitionLevelLabel = (compLevel) => {
+   
+    return `${compLevel} шт.`    
   }
 
-  
+  const scoreLevelScaleText = (score, color) => {    
+    let level = scoreLevelScale(score);
 
-    ymaps.ready(init);
+    if (level == 1) return 'ниже среднего'
+    if (level == 2) return 'средний'
+    if (level == 3) return 'выше среднего'
+    if (level == 4) return 'высокий'
+    if (level == 0) return 'низкий'
+  }
 
-    const getPos = (i) => {
-        let parts = i.split(' ');
+  const scoreLevelScaleLabel = (score, color) => {
+    
+    let level = scoreLevelScale(score);
 
-        return {
-            lat: parts[0],
-            lon: parts[1]
-        }
-    }
 
-    let markers = [];
+    let star = `<svg xmlns="http://www.w3.org/2000/svg" fill="${color}" width="16" height="16" viewBox="0 0 32 32"><path d="M12 .587l3.668 7.568 8.332 1.151-6.064 5.828 1.48 8.279-7.416-3.967-7.417 3.967 1.481-8.279-6.064-5.828 8.332-1.151z"/></svg>`
 
-    const drawMarkers = () => {
+    if (level == 0) return `${star}`
+    if (level == 1) return `${star}${star}`
+    if (level == 2) return `${star}${star}${star}`
+    if (level == 3) return `${star}${star}${star}${star}`
+    if (level == 4) return `${star}${star}${star}${star}${star}`
+
+    
+  }
+
+  const getCompetitionLevel = (item) => {
+    
+    let geo = getPos(item.location);
+    
+    let count = competitionByLocation[`${geo.lat};${geo.lon}`];  
+    return count;  
+    // let compLevel = compScale(count);
         
-        let boundsCollection = new ymaps.GeoObjectCollection();
+    // return compLevel;
+  }
 
-        source.forEach(item => {
-            let geo = getPos(item.location);
-            
-            
-            
-            let marker = new ymaps.Placemark([geo.lat, geo.lon],
-            {
-                balloonContentHeader: item.name,
-                balloonContentBody: `
-                · ${item.okved_name.length > 128 ? item.okved_name.substr(0,128) + '...' : item.okved_name }
-                <br/>· ИНН: ${item.inn}
-                <br/>· ОГРН: ${item.ogrn}
-                <br/>· адрес: ${item.city? item.city+ ', ' : ''} ${item.address}
-                <br/><br/>· <b>score: ${Math.round(item.preds*1000)/1000}</b>
-                <br/><br/><button class='modal-btn' onclick="openModal('${item.inn}')" >Посмотреть детали</button>
-                `
-            }, {
-                hideIconOnBalloonOpen: false,
-                preset: "islands#greenDotIconWithCaption",
-                iconColor: colorFn(item.preds),
-            });
-             
+  
+
+  const calculateCompetitionByGeo = (item) => {
+    let geo = getPos(item.location);
+    
+    const locations = competition[item.okved_code];
+    
+    let count = locations.map(i=> GetGEODistance(geo.lat, geo.lon, i.lat, i.lon))
+      .filter(i=>i<=15).length;
+
+      if (count.length) count--;
+
+      competitionByLocation[`${geo.lat};${geo.lon}`] = count;
+      
+
+  }
+    
+    const initMap = async () => {
+      let myMap = new ymaps.Map("map", {
+          center: [ 47.728732, 41.268128 ],
+          zoom: 7
+      }, {
+          searchControlProvider: 'yandex#search'
+      });
+
+      //  clusterer = new ymaps.Clusterer({
+      //   clusterIconLayout: 'default#pieChart',
+      //   clusterIconPieChartRadius: 35,
+      //   clusterIconPieChartCoreRadius: 25,
+      //   clusterIconPieChartStrokeWidth: 3,
+      //   hasBalloon: true,
+      //     clusterHideIconOnBalloonOpen: false,
+      //     // geoObjectHideIconOnBalloonOpen: false
+      // });
+
+      
+
+      
+
+      
+        
+      window.myMap = myMap;
+    
+      let query = null;
+      query = ymaps.geoQuery(ymaps.regions.load("RU", { lang: "ru" }));
+      query.then(async () => {
+          drawRegionContour(query, "Ростовская область");
+          await loadApp();
+          
+      });
+    
+    }
+    
+    const drawRegionContour = (query, regionName) => {
+    
+      if (query) {
+          let item = query.search(`properties.name = "${regionName}"`);
+    
+          item.setOptions("visible", true);
+          item.setOptions("fillOpacity", 0);
+          item.setOptions("outline", true);
+      
+          item.setOptions("hasBalloon", false);
+          item.setOptions("hasHint", false);
+          item.setOptions("interactivityModel", "default#transparent");
+      
+          item.setOptions("strokeColor", "#3f007d");
+      
+          // item.setOptions("strokeColor", "#999999");
+          item.setOptions("strokeStyle", "shortdash");
+          
+          item.setOptions("strokeWidth", 3);
+          
+      
+          item.each(i => {
+              
+              if (!window.region) window.region = [];
+              window.myMap.geoObjects.add(i);        
+          });
+      }
+      
+    
+      
+    }
+    
+    let markers = [];
+    
+    const drawMarkers = (data, colorFn, scale) => {
+      
+      let boundsCollection = new ymaps.GeoObjectCollection();
+      
+      data.forEach(item => {                        
+          let marker = addMarker(item, colorFn);  
+
+          if (marker) {
               markers.push({
                 item: item,
                 marker: marker
-              });
+              });  
+              boundsCollection.add(marker);
+              
+              markers.push(marker);
+          }
+            
+          
+      })  
+      
+      //clusterer.add(markers.map(i=>i.marker).filter(i=>i));
         
-            boundsCollection.add(marker);
-            //console.log(geo);
-        })    
-
-        //console.log(boundsCollection.length)
-
-        myMap.geoObjects.add(boundsCollection);
-
-        // myMap.setBounds(boundsCollection.getBounds(), { checkZoomRange: true, useMapMargin: true, zoomMargin: 150 });
-        // myMap.geoObjects.remove(boundsCollection);
+      // myMap.geoObjects.add(clusterer);
+      // myMap.setBounds(clusterer.getBounds(), {
+      //     checkZoomRange: true
+      // });
+      myMap.geoObjects.add(boundsCollection);  
     }
 
-    function init() {
-        let myMap = new ymaps.Map("map", {
-            center: [ 47.728732, 41.268128 ],
-            zoom: 7
-        }, {
-            searchControlProvider: 'yandex#search'
-        });
 
-        window.myMap = myMap;
+    const writeLinks = (item) => {
+      let result = ``;
+      if (item.website) {
+        result = ''
+      }
 
-        let query = null;
-        query = ymaps.geoQuery(ymaps.regions.load("RU", { lang: "ru" }));
-        query.then(() => {
-            drawRegionContour(query, "Ростовская область");
-        });
-
-        //drawRegionContour(query, "Ростовская область");
-
-    }
-
-    const drawRegionContour = (query, regionName) => {
-
-        if (query) {
-            let item = query.search(`properties.name = "${regionName}"`);
-
-            item.setOptions("visible", true);
-            item.setOptions("fillOpacity", 0);
-            item.setOptions("outline", true);
-        
-            item.setOptions("hasBalloon", false);
-            item.setOptions("hasHint", false);
-            item.setOptions("interactivityModel", "default#transparent");
-        
-            item.setOptions("strokeColor", "#3388ff");
-        
-            // item.setOptions("strokeColor", "#999999");
-            item.setOptions("strokeStyle", "shortdash");
-            
-            item.setOptions("strokeWidth", 3);
-            
-        
-            item.each(i => {
-                
-                if (!window.region) window.region = [];
-                window.myMap.geoObjects.add(i);        
-            });
+      if (item.website) {
+        let url = item.website.trim();
+        if (url.toLowerCase().indexOf('http') != 0) {
+          url = 'http://' + url
         }
+
+        result = `${result}<br/>· сайт: <a href="${url}" target="_blank">${url}</a>`;
+      } 
+      return result;
+    }
+
+    const getMarkerIcon = (item) => {
+      let okved = item.okved_name.toLowerCase();
+
+      if (okved.indexOf('баз данных')>=0
+      || okved.indexOf('защиты информации')>=0
+      || okved.indexOf('нтернет')>=0
+      || okved.indexOf('компьютер')>=0
+      || okved.indexOf('информационных технологий')>=0
+      || okved.indexOf('программного обеспечения')>=0
+      
+      ) return 'hourglass';
+
+      if (okved.indexOf('науч')>=0  
+        || okved.indexOf('наук')>=0     
+      ) return 'star';
+
+      // if (okved.indexOf('выращ')>=0  
+      //   || okved.indexOf('виног')>=0  
+      //   || okved.indexOf('виног')>=0
+      //   || okved.indexOf('нерафин')>=0
+      //   || okved.indexOf('рафин')>=0
+      //   || okved.indexOf('приправ')>=0
+      //   || okved.indexOf('зерновых культур')>=0
+      // ) return 'leaf';
+
+
+
+      return '';
+    }
+
+    const yearRus = (year) => {
+      let end = year.substring(0,2)*1;
+
+      if (end > 20) {
+        end = (end+"").substring(1,2)*1;
         
+        console.log(end)
+      }
 
-        drawMarkers();
-    }        
-};
+      if (end == 1) return 'год'
+      if (end == 2 || end == 3 || end == 4) return 'года'
+      if (end >= 5 < 20) return 'лет'
+      
+      return 'лет'
+      
+    }
 
-$(document).ready(() => {
-  setTimeout(main, 2500);
-  
-});
+    const yearDiffLabel =(year) => {
+      let diff = new Date().getFullYear()-year*1;
+        if (diff > 0) return `<span class="badge badge-secondary">${diff} ${yearRus(year+'')}</span>`
+        return '';
+    }
+
+    const addMarker = (item, colorFn, scale) => {
+      let geo = getPos(item.location);
+      
+      
+      let color = colorFn(item.preds);
+
+      let level = getCompetitionLevel(item);
+      // <br/>${scoreLevelScaleLabel(item.preds, color)}
+      let marker = new ymaps.Placemark([geo.lat, geo.lon],
+        {
+            balloonContentHeader: `${item.name}${item.city ? ` (` + item.city +')' : ''}, основана в&nbsp;${item.create_year} ${yearDiffLabel(item.create_year)}
+            `,
+            balloonContentBody: `
+            <b>${item.okved_name.length > 128 ? item.okved_name.substr(0,128) + '...' : item.okved_name }</b> 
+            <br/><span >· организаций с таким же ОКВЭД в радиусе 15 км: ${getCompetitionLevelLabel(level)}</span>
+            <br/>
+            <br/>· Рубрика: ${item.rubric}
+            <br/>· Подрубрика: ${item.sub_rubric}            
+            <br/>· ИНН: ${item.inn}
+            <br/>· ОГРН: ${item.ogrn}
+            <br/>· ОКВЭД: ${item.okved_code}
+            <br/>
+                        
+            <br/>· адрес: ${item.city? item.city+ ', ' : ''} ${item.address}            
+            ${writeLinks(item)}
+            <br/><br/>
+            <svg width="16" height="16">
+              <rect width="16" height="16" style="fill:${color};" />
+            </svg>&nbsp;
+            <b>Innovation score: ${Math.round(item.preds*1000)/1000}</b>&nbsp; (${scoreLevelScaleText(item.preds, color)})
+            <br/>
+            <br/><a class='my-primary-btn'
+             href="https://hackathon.graphometrica.ai/explain/?inn=${item.inn}"
+             target="_blank">Почему такой innovation score? →</a>
+            `
+        }, {
+            hideIconOnBalloonOpen: false,
+            preset: 'islands#glyphIcon',
+            iconGlyph: getMarkerIcon(item),
+            iconGlyphColor: colorFn(item.preds),
+            iconColor: colorFn(item.preds),
+        });
+
+      //   marker.events.add('click', function () {
+      //     getCompetition(item, marker)
+      // });
+
+        return marker;
+    }
+    
+    const normalizeBackendData = (data) => {      
+      
+      let result = data.filter(i=>i.location)
+        .map(i=> {
+          let location = i.location;
+          
+          if (location.indexOf(', ') >= 0) location = location.split(', ')[0]
+          return {...i, location: location}
+        })
+        
+      return result;
+    }
+
+    const filterByLowPreds = (data) => {
+      return data.filter(i=> i.preds > 0.1).sort((a,b) => a.preds-b.preds)
+    }
+    
+    
+    const getPos = (i) => {
+      let parts = i.split(' ');
+    
+      return {
+          lat: parts[0],
+          lon: parts[1]
+      }
+    }
+    
+    const loadApp = async () => {
+    
+      
+    let raw = await fetch('https://hackathon.graphometrica.ai/api/getAll')
+    .then(r => r.json())
+    
+      let data = normalizeBackendData(raw);      
+      calculateCompetition(data);
+      data = filterByLowPreds(data);
+    
+      let preds = [...new Set(data.map(i=>i.preds))]
+    
+      const predMin = d3.min(preds)
+      const predMax = d3.max(preds)
+
+      
+      
+      // const colorFn = d3.scaleSequential()
+      //   .interpolator(d3.interpolatePurples)
+      //   .domain([predMin, predMax ]);
+
+      const colorFn = d3.scaleSequential()
+      .domain([predMin, predMax])
+      .interpolator(d3.interpolate("#dfcfe6", "#430775"));
+//3f007d
+        var scale = d3.scaleLinear()
+          .domain([predMin, predMax])
+          .range([0, 1]);
+
+          scoreLevelScale  = d3.scaleLinear()
+        .domain([scale(predMin), scale(predMax)])
+        .rangeRound([0, 4])
+
+          data.forEach(i=>i.preds = scale(i.preds))
+        
+        drawMarkers(data, colorFn, scale);         
+    };
+    
+    $(document).ready(() => {
+      ymaps.ready(initMap);    
+    });
+
+})();
+
---
+++ utils.js
@@ -0,0 +1,103 @@
+'use strict';
+function Median(data) {
+  return Quartile_50(data);
+}
+
+function Quartile_25(data) {
+  return Quartile(data, 0.25);
+}
+
+function Quartile_50(data) {
+  return Quartile(data, 0.5);
+}
+
+function Quartile_75(data) {
+  return Quartile(data, 0.75);
+}
+
+function Quartile(data, q) {
+  data=Array_Sort_Numbers(data);
+  var pos = ((data.length) - 1) * q;
+  var base = Math.floor(pos);
+  var rest = pos - base;
+  if( (data[base+1]!==undefined) ) {
+    return data[base] + rest * (data[base+1] - data[base]);
+  } else {
+    return data[base];
+  }
+}
+
+function Array_Sort_Numbers(inputarray){
+  return inputarray.sort(function(a, b) {
+    return a - b;
+  });
+}
+
+function Array_Sum(t){
+   return t.reduce(function(a, b) { return a + b; }, 0); 
+}
+
+function Array_Average(data) {
+  return Array_Sum(data) / data.length;
+}
+
+function Array_Stdev(tab){
+   var i,j,total = 0, mean = 0, diffSqredArr = [];
+   for(i=0;i<tab.length;i+=1){
+       total+=tab[i];
+   }
+   mean = total/tab.length;
+   for(j=0;j<tab.length;j+=1){
+       diffSqredArr.push(Math.pow((tab[j]-mean),2));
+   }
+   return (Math.sqrt(diffSqredArr.reduce(function(firstEl, nextEl){
+            return firstEl + nextEl;
+          })/tab.length));  
+}
+
+function RGBToHex(rgb) {
+    
+    let sep = rgb.indexOf(",") > -1 ? "," : " ";
+    
+    rgb = rgb.substr(4).split(")")[0].split(sep);
+  
+    let r = (+rgb[0]).toString(16),
+        g = (+rgb[1]).toString(16),
+        b = (+rgb[2]).toString(16);
+  
+    if (r.length == 1)
+      r = "0" + r;
+    if (g.length == 1)
+      g = "0" + g;
+    if (b.length == 1)
+      b = "0" + b;
+  
+    return "#" + r + g + b;
+  }
+
+  const GetGEODistance = (lat1, lon1, lat2, lon2, unit) => {
+    if (lat1 == lat2 && lon1 == lon2) {
+      return 0;
+    } else {
+      let radlat1 = (Math.PI * lat1) / 180;
+      let radlat2 = (Math.PI * lat2) / 180;
+      let theta = lon1 - lon2;
+      let radtheta = (Math.PI * theta) / 180;
+      let dist =
+        Math.sin(radlat1) * Math.sin(radlat2) +
+        Math.cos(radlat1) * Math.cos(radlat2) * Math.cos(radtheta);
+      if (dist > 1) {
+        dist = 1;
+      }
+      dist = Math.acos(dist);
+      dist = (dist * 180) / Math.PI;
+      dist = dist * 60 * 1.1515;
+      if (!unit || unit == "K") {
+        dist = dist * 1.609344;
+      }
+      if (unit == "N") {
+        dist = dist * 0.8684;
+      }
+      return dist;
+    }
+  };
